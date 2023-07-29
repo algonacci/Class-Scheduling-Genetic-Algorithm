8ad6f1ec0961ef18b6a594234cf48ecc8dc8c23c
Eric Julianto <algonacci1@gmail.com>
2023-07-29 13:52:00 +0700

    Finishing

--- app.py
+++ app.py
@@ -1,12 +1,159 @@
-from flask import Flask, request, render_template
-import pandas
+import matplotlib.pyplot as plt
+import pandas as pd
+from flask import Flask, render_template, request
+
+import module as md
+
+plt.switch_backend('agg')
+
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/schedule", methods=["POST"])
+def schedule():
     if request.method == "POST":
-        pass
-    else:
-        return render_template("index.html")
+        df_dataset_T = pd.read_excel(request.files["file1"])
+        df_dataset_P = pd.read_excel(request.files["file2"])
+        df_ruangan = pd.read_excel(request.files["file3"])
+        df_hari = pd.read_excel(request.files["file4"])
+        df_jam = pd.read_excel(request.files["file5"])
+
+        ruangan_list = df_ruangan.to_dict(orient='records')
+        array_ruangan = [d['Ruangan'] for d in ruangan_list]
+
+        hari_list = df_hari.to_dict(orient='records')
+        array_hari = [h['Hari'] for h in hari_list]
+
+        grouped_jam = df_jam.groupby(" Durasi")["Jam"].apply(list)
+        mapped_jam = grouped_jam.to_dict()
+
+        courses_T = md.generate_course_list(
+            df_dataset_T, array_ruangan, array_hari, mapped_jam)
+        courses_P = md.generate_course_list(
+            df_dataset_P, array_ruangan, array_hari, mapped_jam)
+
+        population = md.generate_schedule(courses_T)
+
+        best_schedule_T, best_fitness_per_generation_T = md.genetic_algorithm(
+            courses_T, 100, 2)
+        best_schedule_P, best_fitness_per_generation_P = md.genetic_algorithm(
+            courses_P, 100, 2)
+
+        plt.plot(best_fitness_per_generation_T)
+        plt.xlabel("Generation")
+        plt.ylabel("Best Fitness")
+        plt.title("Best Fitness per Generation For Theory")
+        plt.savefig("static/Theory.png")
+
+        plt.plot(best_fitness_per_generation_P)
+        plt.xlabel("Generation")
+        plt.ylabel("Best Fitness")
+        plt.title("Best Fitness per Generation for Practicum")
+        plt.savefig("static/Practicum.png")
+
+        for value in population:
+            print(value.kode_matakuliah)
+
+        days_order = {'Senin': 1, 'Selasa': 2,
+                      'Rabu': 3, 'Kamis': 4, 'Jumat': 5}
+
+        best_schedule_T.sort(key=lambda x: (days_order[x.day], x.time))
+        courses_data_T = []
+        for course in best_schedule_T:
+            print('---------------------')
+            print(f'Kode matakuliah\t:{course.kode_matakuliah}')
+            print(f'Nama matakuliah\t:{course.nama_matakuliah}')
+            print(f'Jenis\t\t:{course.jenis}')
+            print(f'Jam\t\t:{course.time}')
+            print(f'Hari\t\t:{course.day}')
+            print(f'Ruangan\t\t:{course.room}')
+            print(f'Dosen1\t\t:{course.lecturer1}')
+            print(f'Dosen2\t\t:{course.lecturer2}')
+            print(f'Asisten Dosen1\t:{course.assistant1}')
+            print(f'Asisten Dosen2\t:{course.assistant2}')
+            print(f'Kelas\t\t:{course.kelas}')
+            print(f'Semester\t:{course.semester}')
+            print('---------------------')
+            course_dict = {
+                'Kode matakuliah': course.kode_matakuliah,
+                'Nama matakuliah': course.nama_matakuliah,
+                'Jenis': course.jenis,
+                'Jam': course.time,
+                'Hari': course.day,
+                'Ruangan': course.room,
+                'Dosen1': course.lecturer1,
+                'Dosen2': course.lecturer2,
+                'Asisten Dosen1': course.assistant1,
+                'Asisten Dosen2': course.assistant2,
+                'Kelas': course.kelas,
+                'Semester': course.semester
+            }
+            courses_data_T.append(course_dict)
+
+        days_order = {'Senin': 1, 'Selasa': 2,
+                      'Rabu': 3, 'Kamis': 4, 'Jumat': 5}
+        best_schedule_P.sort(key=lambda x: (days_order[x.day], x.time))
+        courses_data_P = []
+        for course in best_schedule_P:
+            print('---------------------')
+            print(f'Kode matakuliah\t:{course.kode_matakuliah}')
+            print(f'Nama matakuliah\t:{course.nama_matakuliah}')
+            print(f'Jenis\t\t:{course.jenis}')
+            print(f'Jam\t\t:{course.time}')
+            print(f'Hari\t\t:{course.day}')
+            print(f'Ruangan\t\t:{course.room}')
+            print(f'Dosen1\t\t:{course.lecturer1}')
+            print(f'Dosen2\t\t:{course.lecturer2}')
+            print(f'Asisten Dosen1\t:{course.assistant1}')
+            print(f'Asisten Dosen2\t:{course.assistant2}')
+            print(f'Kelas\t\t:{course.kelas}')
+            print(f'Semester\t:{course.semester}')
+            print('---------------------')
+            course_dict = {
+                'Kode matakuliah': course.kode_matakuliah,
+                'Nama matakuliah': course.nama_matakuliah,
+                'Jenis': course.jenis,
+                'Jam': course.time,
+                'Hari': course.day,
+                'Ruangan': course.room,
+                'Dosen1': course.lecturer1,
+                'Dosen2': course.lecturer2,
+                'Asisten Dosen1': course.assistant1,
+                'Asisten Dosen2': course.assistant2,
+                'Kelas': course.kelas,
+                'Semester': course.semester
+            }
+            courses_data_P.append(course_dict)
+
+        df_predict = pd.DataFrame(courses_data_T + courses_data_P)
+
+        df_predict_T = pd.DataFrame(courses_data_T)
+        df_predict_T = df_predict[['Jam', 'Hari', 'Semester', 'Kode matakuliah', 'Nama matakuliah', 'Jenis', 'Ruangan',
+                                   'Dosen1', 'Dosen2', 'Asisten Dosen1', 'Asisten Dosen2', 'Kelas',]]
+        df_predict_T.sort_values(by=['Jam', 'Semester', 'Hari'])
+
+        df_predict_P = pd.DataFrame(courses_data_P)
+        df_predict_P = df_predict_P[['Jam', 'Hari', 'Semester', 'Kode matakuliah', 'Nama matakuliah', 'Jenis', 'Ruangan',
+                                     'Dosen1', 'Dosen2', 'Asisten Dosen1', 'Asisten Dosen2', 'Kelas',]]
+        df_predict_P.sort_values(by=['Jam', 'Semester', 'Hari'])
+
+        df_predict = df_predict.to_html(
+            index=False, classes="table table-responsive table-striped")
+        df_predict_T = df_predict_T.to_html(
+            index=False, classes="table table-responsive table-striped")
+        df_predict_P = df_predict_P.to_html(
+            index=False, classes="table table-responsive table-striped")
+
+        return render_template("schedule.html",
+                               theory_graph="static/Theory.png",
+                               practicum_graph="static/Practicum.png",
+                               df_predict=df_predict,
+                               df_predict_T=df_predict_T,
+                               df_predict_P=df_predict_P
+                               )
--- module.py
+++ module.py
@@ -1,3 +1,7 @@
+import random
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+import datetime
 import pandas as pd
 
 
@@ -18,3 +22,183 @@ class Course:
         self.time = None
         self.room = None
         self.day = None
+
+
+def generate_course_list(dataset, array_ruangan, array_hari, grouped_jam):
+    courses = []
+    for _, row in dataset.iterrows():
+        for key, val in grouped_jam.items():
+            if key == row['Jam']:
+                if row['Jenis'] == 'Teori':
+                    days = ['Senin', 'Selasa']
+                else:
+                    days = ['Rabu', 'Kamis', 'Jumat']
+                courses.append(Course(row['Kode'], row['Mata Kuliah'], row['Jenis'], val, days, array_ruangan, row['Dosen1'],
+                               row['Dosen2'], row['Asisten Dosen1'], row['Asisten Dosen2'], row['Kelas'], row['Semester']))
+    return courses
+
+
+def is_overlap(times):
+    datetime_slots = []
+    for time_slot in times:
+        start, end = time_slot.split('-')
+        start = datetime.datetime.strptime(start.strip(), '%H:%M')
+        end = datetime.datetime.strptime(end.strip(), '%H:%M')
+        datetime_slots.append((start, end))
+
+    for i in range(len(datetime_slots)):
+        for j in range(i+1, len(datetime_slots)):
+            start1, end1 = datetime_slots[i]
+            start2, end2 = datetime_slots[j]
+            if (start1 <= start2 and start2 < end1) or (start2 <= start1 and start1 < end2):
+                return True
+    return False
+
+
+def generate_hash(schedule):
+    return hash(tuple((course.kode_matakuliah, course.time, course.day, course.room) for course in schedule))
+
+
+def parse_time_string(time_string):
+    start_time_str, end_time_str = time_string.split("-")
+    start_time = datetime.datetime.strptime(start_time_str, "%H:%M").time()
+    end_time = datetime.datetime.strptime(end_time_str, "%H:%M").time()
+    return start_time, end_time
+
+
+def fitness_func(schedule):
+    conflicts = 0
+    n = len(schedule)
+
+    for i in range(n):
+        course1 = schedule[i]
+        for j in range(i + 1, n):
+            course2 = schedule[j]
+
+            overlap = is_overlap([course1.time, course2.time])
+            same_day = course1.day == course2.day
+            same_semester = course1.semester == course2.semester
+            same_room = course1.room == course2.room
+            same_lecturer1 = course1.lecturer1 == course2.lecturer1
+            same_lecturer2 = course1.lecturer2 == course2.lecturer2
+            same_assistant1 = course1.assistant1 == course2.assistant1
+            same_assistant2 = course1.assistant2 == course2.assistant2
+            same_subject = course1.nama_matakuliah == course2.nama_matakuliah
+            same_subject_type = course1.jenis == course2.jenis
+            different_semester = course1.semester == course2.semester
+
+            conflict = False
+            if overlap and same_day and different_semester:
+                conflict = True
+
+            if conflict:
+                conflicts += 1
+                break
+    fitness_result = 1 / (1 + conflicts)
+    return fitness_result
+
+
+def generate_schedule(courses):
+    schedule = []
+    for course in courses:
+        time = random.choice(course.times)
+        day = random.choice(course.days)
+        code = course.kode_matakuliah
+        lecturer1 = course.lecturer1
+        lecturer2 = course.lecturer2
+        assistant1 = course.assistant1
+        assistant2 = course.assistant2
+        subject_name = course.nama_matakuliah
+        kelas = course.kelas
+        semester = course.semester
+        jenis = course.jenis
+
+        if jenis == 'Teori':
+            available_rooms = ['GD515', 'GD516']
+        elif jenis == 'Praktikum':
+            available_rooms = ['GD513514', 'GD525526', 'GD714', 'GD723']
+        else:
+            available_rooms = []
+
+        room = random.choice(available_rooms)
+
+        course_obj = Course(code, subject_name, jenis, course.times, course.days,
+                            course.rooms, lecturer1, lecturer2, assistant1, assistant2, kelas, semester)
+        course_obj.time = time
+        course_obj.room = room
+        course_obj.day = day
+        schedule.append(course_obj)
+    return schedule
+
+
+def mutate(schedule):
+    course = random.choice(schedule)
+    course.time = random.choice(course.times)
+    course.room = random.choice(course.rooms)
+    course.day = random.choice(course.days)
+    return schedule
+
+
+def crossover(schedule1, schedule2):
+    crossover_point = random.randint(1, len(schedule1) - 1)
+    new_schedule = schedule1[:crossover_point] + schedule2[crossover_point:]
+    return new_schedule
+
+
+def select_parents(population):
+    total_fitness = sum([fitness_func(schedule) for schedule in population])
+    probabilities = [fitness_func(
+        schedule) / total_fitness for schedule in population]
+    parent1, parent2 = random.choices(population, probabilities, k=2)
+    return parent1, parent2
+
+
+def create_child(population):
+    parent1, parent2 = select_parents(population)
+    child = crossover(parent1, parent2)
+    if random.random() < 0.6:
+        mutate(child)
+    return child
+
+
+def create_children(population, num_children):
+    with ProcessPoolExecutor() as executor:
+        return list(executor.map(create_child, [population] * num_children))
+
+
+def genetic_algorithm(courses, population_size, generations):
+    population = [generate_schedule(courses) for _ in range(population_size)]
+    fitness_cache = [(generate_hash(schedule), fitness_func(schedule))
+                     for schedule in population]
+
+    best_fitness_per_generation = []
+    for gen in range(generations):
+        print(f"Gen {gen + 1} processing...")
+        new_population = []
+
+        elitism_size = int(population_size * 0.1)
+        new_population.extend(sorted(population, key=lambda x: [
+                              y[1] for y in fitness_cache if y[0] == generate_hash(x)][0], reverse=True)[:elitism_size])
+
+        num_children = population_size - elitism_size
+        children = create_children(population, num_children)
+        new_population.extend(children)
+        new_population_with_fitness = []
+
+        for child in new_population:
+            child_key = generate_hash(child)
+
+            if child_key in [x[0] for x in fitness_cache]:
+                fitness = [x[1] for x in fitness_cache if x[0] == child_key][0]
+            else:
+                fitness = fitness_func(child)
+                fitness_cache.append((child_key, fitness))
+
+            new_population_with_fitness.append((child, fitness))
+
+        new_population_with_fitness.sort(key=lambda x: x[1], reverse=True)
+        population = [x[0] for x in new_population_with_fitness]
+        best_fitness_per_generation.append(new_population_with_fitness[0][1])
+
+    best_schedule = max(population, key=fitness_func)
+    return best_schedule, best_fitness_per_generation
